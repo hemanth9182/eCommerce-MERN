7070878b307c40c569575496d03d426e581f2444
Hemanth9182 <mohinpasha216@gmail.com>
2024-04-22 10:32:28 +0530

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 
 const App = () => {
   return (
-    <div>photoshop</div>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome To Proshop</h1>
+        </Container>
+      </main>
+     <Footer/>
+    </>
+  );
+};
 
 export default App;
-
